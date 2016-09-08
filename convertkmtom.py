1c7b7a9c15ba1edbd148145da5aa8751f8f09b64
jessebridge <jesse.bridge@my.jcu.edu.au>
2016-09-08 13:33:26 +1000

    add completed version

--- convertkmtom.py
+++ convertkmtom.py
@@ -8,7 +8,13 @@ class convert(App):
         self.title = "convert m to km"
         self.root = Builder.load_file('convertkmtom.kv')
         return self.root
-    def handle_calculate(self, value):
-        result = value ** 2
+    def handle_calculate_positive(self, value):
+        result = value + 1
+        self.root.ids.input_number.text = str(result)
+    def handle_calculate_negative(self, value):
+        result = value - 1
+        self.root.ids.input_number.text = str(result)
+    def handle_convert(self,value):
+        result = value / 1000
         self.root.ids.output_label.text = str(result)
 convert().run()
